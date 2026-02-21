072158d80154902a1db661b1db54db1017fdaf29
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-02-20 22:02:49 -0500

    Update 5-5. Alien Colors.py
    added if elif else routine

--- 5-5. Alien Colors.py
+++ 5-5. Alien Colors.py
@@ -1,7 +1,12 @@
-alien_color = 'green'
-#alien_color = 'yellow'
+alien_colors =['green', 'yellow', 'red']
 
-if alien_color == 'green':
-    print("You just earned 5 points!")
-elif alien_color != 'green':
-    print("You just earned 10 points!")
+for alien_color in alien_colors:
+    if alien_color == 'green':
+        print("You just earned 5 points!")
+    elif alien_color == 'yellow':
+        print("You just earned 10 points!")
+    else: 
+        print("You just earned 15 points!")
+
+    
+
