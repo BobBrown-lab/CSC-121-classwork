247d17db10cd520e3390b8a026dbd128bb02d8d1
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-02-20 22:48:39 -0500

    Update 5-5. Alien Colors.py
    completed

--- 5-5. Alien Colors.py
+++ 5-5. Alien Colors.py
@@ -8,5 +8,35 @@ for alien_color in alien_colors:
     else: 
         print("You just earned 15 points!")
 
+alien_colors =['green', 'yellow', 'red']
+
+for alien_color in alien_colors:
+    if alien_color == 'green':
+        print("\nYou just earned 5 points!")
+    if alien_color == 'yellow':
+        print("You just earned 10 points!")
+    if alien_color == 'red':
+        print("You just earned 15 points!")
+
+for alien_color in alien_colors:  
+    points = ()      
+    if alien_color == 'green':
+        points = 5
+        print(f"\nYou just earned {points} points!")
+    elif alien_color == 'yellow':
+        points = 10
+        print(f"You just earned {points} points!")
+    elif alien_color == 'red':
+        points = 15
+        print(f"You just earned {points} points!")
+
+
+
+
+
+
     
+
+    
+
 
