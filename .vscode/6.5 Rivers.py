7754cdcf77ab6b1d11608d4b6b9dcd8341fbfe6e
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-02-26 01:56:53 -0500

    v.1
    satisfies assignment

--- .vscode/6.5 Rivers.py
+++ .vscode/6.5 Rivers.py
@@ -1,3 +1,9 @@
 rivers = {'Egypt': 'Nile', 'USA': 'Mississippi', 'Ukraine': 'Dnieper'}
 for country, river in rivers.items():
     print(f"The {river} runs through {country}.")
+
+# separate output
+print ('\n')
+       
+for country in rivers.keys():
+    print(country)
