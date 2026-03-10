dbf93b7b1fde434de672def64a2c4f134508c5ca
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-03-09 20:47:50 -0400

    Update 7-5. Movie Tickets.py
    corrected for assignment

--- .vscode/7-5. Movie Tickets.py
+++ .vscode/7-5. Movie Tickets.py
@@ -1,11 +1,21 @@
 # Get number of tickets
-
 num_tickets = int(input("How many tickets do you want to buy? "))
-    
-tier_1 = int(input("How many tickets for children under 3? "))
-tier_2 = int(input("How many tickets for children between 3 and 12? "))
-tier_3 = int(input("How many tickets for those over 12? "))
 
-total = (tier_2*10) + (tier_3*15)
+#get ages of ticket holders
+ages = []
+for age in range(num_tickets):
+    age = int(input("Enter the age of the person for this ticket: "))
+    ages.append(age)
 
-print(f"The total cost of your tickets is ${total}.")
+subtot = int() #variable to hold the subtotal of the ticket costs 
+
+for age in ages:
+    if age < 3:
+        subtot += 0
+    elif age <= 12:
+        subtot += 10
+    else:
+        subtot += 15
+
+#total = subtotal
+print(f"The total cost of your tickets is ${subtot}.")
