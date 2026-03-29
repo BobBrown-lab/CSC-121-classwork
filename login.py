3865b498a246fbfdf8789ed91a4061c5a3db4c63
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-03-29 06:42:46 -0400

    users.py
    close to done

--- login.py
+++ login.py
@@ -1,32 +1,46 @@
 class User:
     """describe user"""
 
-    def __init__(self, first_name, last_name, sex, login_attempts=0):
+    def __init__(self, first_name, last_name, sex):
         """user attributes"""
        
         self.first_name = first_name
         self.last_name = last_name
         self.sex = sex
-        self.login_attempts = login_attempts
 
-    def increment_login_attempts(login_attempts):
-        me.login_attempts += 1
-        print(me.login_attempts)
+        """login attributes"""
+        self.login_attempts = 0
 
+    """increment login attempts"""   
+    def increment_login_attempts(self):
+        self.login_attempts += 1
 
-        me.increment_login_attempts
-        me.increment_login_attempts
-        me.increment_login_attempts
+    """reset login attempts"""
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+user = User('bob', 'brown', 'm')
+
+        
+"""call increment 3 times"""
+user.increment_login_attempts
+user.increment_login_attempts
+user.increment_login_attempts
+
+"""print login attempts"""
+print(f"Login attempts: {user.login_attempts}")
+
+"""Reset and print login attempts"""
+user.reset_login_attempts()
+print(f"After reset: {user.login_attempts}")
+
      
-     
-me = User('bob', 'brown', 'm', 0)
 
 print (
-    f"\n{me.first_name.title()}"
-    f" {me.last_name.title()}"
-    f" {me.sex}"
-    f" {me.login_attempts}"
-    "\n"
+    f"\n{user.first_name.title()}"
+    f" {user.last_name.title()}"
+    f" {user.sex}"
+     "\n"
     )
     
 
