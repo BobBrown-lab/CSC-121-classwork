2f2dd4c7f3f89934f1aafc475b09a31ece321156
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-04-05 00:00:41 -0400

    python scratch pad
    random

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,19 @@
 from pathlib import Path
 
-path = Path('pi_digits.txt')
-contents = path.read_text()
-print(contents)
+# path = Path('pi_digits.txt')
+#contents = path.read_text()
+# lines = contents.splitlines()
+# pi_string = ''
+# for line in lines:
+#     #print(line)
+#     pi_string += line
+# print(pi_string)
+# print(len(pi_string))    
+# print(contents)
+
+path = Path('programming.txt')
+path.write_text("I love programming")
+contents = "I love programming.\n"
+contents += "I love creating new games.\n"
+contents += "I also love working with data.\n"
+path.write_text(contents)
