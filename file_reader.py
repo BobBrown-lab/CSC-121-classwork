bb3e4e2c1d0ceb50820dba092d0acccd12fceeba
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-04-06 23:24:05 -0400

    10-4. Guestbook
    final

--- file_reader.py
+++ file_reader.py
@@ -11,9 +11,19 @@ from pathlib import Path
 # print(len(pi_string))    
 # print(contents)
 
-path = Path('programming.txt')
-path.write_text("I love programming")
-contents = "I love programming.\n"
-contents += "I love creating new games.\n"
-contents += "I also love working with data.\n"
+# path = Path('programming.txt')
+# #path.write_text("I love programming")
+# contents = "I love programming.\n"
+# contents += "I love creating new games.\n"
+# contents += "I also love working with data.\n"
+# path.write_text(contents)
+
+import json
+numbers = [2,3,5,7,11,13]
+path = Path('numbers.json')
+contents = json.dumps(numbers)
 path.write_text(contents)
+contents = path.read_text()
+numbers = json.loads(contents)
+
+print(numbers)
