240b93374d39ac50b57f7df29bcf2f981ab0b4c9
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-04-13 17:16:06 -0400

    revised

--- test_name_function.py
+++ test_name_function.py
@@ -3,6 +3,6 @@ from name_function import get_formatted_name
 
 def test_first_last_name():
     """test for average name"""
-    formatted_name = get_formatted_name('bob, brown')
+    formatted_name = get_formatted_name('bob', 'brown')
     assert formatted_name == 'Bob Brown'
 
