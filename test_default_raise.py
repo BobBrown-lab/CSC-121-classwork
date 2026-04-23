1a4a71e7688065558515186ed0fb4964f213e7c8
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-04-22 22:30:48 -0400

    test_default_raise
    final

--- test_default_raise.py
+++ test_default_raise.py
@@ -1,4 +1,3 @@
-
 from employee import Employee
 
 def test_give_default_raise():
@@ -6,9 +5,3 @@ def test_give_default_raise():
     emp = Employee('john','doe', 30_000)
     emp.give_raise()
     assert emp.salary == 35000
-
-def test_give_custom_raise():
-    """test for custom 6_000 raise"""
-    emp = Employee('jane', 'doe', 35000)
-    emp.give_raise(6000)
-    assert emp.salary ==41000
