a437fcf7cd9c89aab9ee51b2c7800249e812e3ba
BobBrown-lab <roberthbrown@students.abtech.edu>
2026-04-23 23:33:54 -0400

    Update test_employee.py
    corrected

--- test_employee.py
+++ test_employee.py
@@ -3,15 +3,19 @@ import pytest
 from employee import Employee
 
 @pytest.fixture
+def default_employee():
+    return Employee('john','doe', 30_000)
 
-def test_give_default_raise():
+@pytest.fixture
+def custom_employee():
+    return Employee('jane', 'doe', 35_000)  
+
+def test_give_default_raise(default_employee):
     """test for default 5_000 raise"""
-    emp = Employee('john','doe', 30_000)
-    emp.give_raise()
-    assert emp.salary == 35000
+    default_employee.give_raise()
+    assert default_employee.salary == 35000
 
-def test_give_custom_raise():
+def test_give_custom_raise(custom_employee):
     """test for custom 6_000 raise"""
-    emp = Employee('jane', 'doe', 35000)
-    emp.give_raise(6000)
-    assert emp.salary ==41000
+    custom_employee.give_raise(6000)
+    assert emp.salary == 41000
